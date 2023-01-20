507f9b05b1c0679405f4a8850a1dfb6e0d6095a3
Ruanpp <ruanpablosilvap07@gmail.com>
2023-01-20 12:27:42 -0300

    search by title

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 const route = Router();
 
-import { create, findAll, topNews,  findById } from "../controllers/news.controller.js"
+import { create, findAll, topNews, findById, searchByTitle } from "../controllers/news.controller.js"
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 route.post("/", authMiddleware, create);
 route.get("/", findAll);
-route.get("/top", topNews);
-route.get("/:id", findById);
+route.get("/top", topNews); 
+route.get("/search", searchByTitle);
+
+
+
+route.get("/:id", authMiddleware, findById);
 
 export default route;
